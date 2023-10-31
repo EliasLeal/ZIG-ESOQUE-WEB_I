5d14b6354da62a8b95c819885276f20b168aa252
EliasLeal <57503668+EliasLeal@users.noreply.github.com>
2023-10-31 16:01:17 -0300

    Commit Script 1

---
+++ frontend/src/styles/global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'poppins', sans-serif;
+  }
+  
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: #add8e6;
+  }
+`;
+
+export default Global;
